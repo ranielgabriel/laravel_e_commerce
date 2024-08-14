923e6c0b8547b845ec0b8c4ff31b81f99a1d6117
Raniel Gabriel <ranielramosgabriel@gmail.com>
2024-08-14 14:57:42 +0800

    Models

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -9,20 +10,26 @@ class DashboardController extends Controller
     //
     public function index()
     {
-        // Array
-        $products = [
-            [
-                "name" => "Nike Air Max",
-                "image" => "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8adace69-12cb-47fb-8921-661a68166a33/WMNS+AIR+FORCE+1+%2707+LX.png",
-                "description" => "Running Shoes ni Yulo"
-            ],
-            [
-                "name" => "Jordan",
-                "image" => "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8adace69-12cb-47fb-8921-661a68166a33/WMNS+AIR+FORCE+1+%2707+LX.png",
-                "description" => "Running Shoes ni Jordan"
-            ],
-        ];
+        // Pang create ng product
+        $product = new Product();
+        $product->name = "Nike Air Max";
+        $product->image = "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8adace69-12cb-47fb-8921-661a68166a33/WMNS+AIR+FORCE+1+%2707+LX.png";
+        $product->description = "Running Shoes ni Yulo";
+        $product->save();
 
-        return view('dashboard')->with("products", json_encode($products));
+        // Pang display ng product
+        $products = Product::get();
+        return view('dashboard')->with('products', $products);
+
+        // // Array
+        // $products = [
+        //     [
+        //         "name" => "Nike Air Max",
+        //         "image" => "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8adace69-12cb-47fb-8921-661a68166a33/WMNS+AIR+FORCE+1+%2707+LX.png",
+        //         "description" => "Running Shoes ni Yulo"
+        //     ]
+        // ];
+
+        // return view('dashboard')->with("products", json_encode($products));
     }
 }
